9ef78db87bfa5f0e35a024c6c093674874d8090f
Ana Sauras Fernández <118003485+asaurasieu@users.noreply.github.com>
2025-11-10 14:40:39 +0100

    Sender Code

---
+++ send.cpp
@@ -0,0 +1,29 @@
+#include "MicroBit.h" 
+
+MicroBit uBit; 
+
+int main()
+{
+	uBit.init(); 
+	uBit.radio.enable(); 
+	
+	PacketBuffer b(1);
+	
+	while(1)
+	{
+	
+	  b[0] = 0; 
+	  if (uBit.buttonA.isPressed())
+	      b[0] = 1;  
+	      
+	  else if (uBit.buttonB.isPressed())
+	  	b[0] = 2; 
+	  	
+	  
+	  
+	  uBit.radio.datagram.send(b);	
+	  uBit.sleep(100); 
+	  
+	 }
+}
+
